d64e2049ce7a74e36afb3121410cf91f43bc0028
Abril <abrilbazan28@gmail.com>
2022-08-25 13:13:41 -0300

    first function

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 // document.getElementById("count-el").innerText=5
 // cmd + k + c
-// store data with variables
-let firstBatch = 5
-let secondBatch = 7
-let count = firstBatch * secondBatch
-console.log(count)
+// let contar = 5
+// contar = contar + 1
+// console.log(contar)
 
+// init the count as 0
+// listen for clicks on the increment button
+//increment the count variable when the button in clicked
+//change the count-el in the HTML to reflect the new count
 
-let myAge = 19
-let humanDogRatio= 7
-let myDogAge= myAge * humanDogRatio
-
-console.log(myDogAge)
-
-let contar = 5
-// contar + 1
-contar = contar + 1
-contar = contar + 10
-contar=contar-1 
-console.log(contar)
-
-let bonusPoints=50
-bonusPoints=bonusPoints+50
-bonusPoints=bonusPoints-75
-bonusPoints=bonusPoints+45
-console.log(bonusPoints)
+function increment(){
+    console.log("The button was clicked")
+}
