618fe642c58680ad6f7190ae0acd2d97a435bbcc
Abril <abrilbazan28@gmail.com>
2022-08-25 13:00:11 -0300

    practicing variables in js

---
+++ index.js
@@ -0,0 +1,27 @@
+// document.getElementById("count-el").innerText=5
+// cmd + k + c
+// store data with variables
+let firstBatch = 5
+let secondBatch = 7
+let count = firstBatch * secondBatch
+console.log(count)
+
+
+let myAge = 19
+let humanDogRatio= 7
+let myDogAge= myAge * humanDogRatio
+
+console.log(myDogAge)
+
+let contar = 5
+// contar + 1
+contar = contar + 1
+contar = contar + 10
+contar=contar-1 
+console.log(contar)
+
+let bonusPoints=50
+bonusPoints=bonusPoints+50
+bonusPoints=bonusPoints-75
+bonusPoints=bonusPoints+45
+console.log(bonusPoints)
